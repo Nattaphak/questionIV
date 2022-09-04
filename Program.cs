e8c3f5c5ebc0cab361e2dc926c94cc16ba794b5a
Nattaphak Udomwasurat <nattaphak.udom@kmutt.ac.th>
2022-09-04 18:50:30 +0700

    questionIV

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a; 
+        int.TryParse(Console.ReadLine(), out a);
+
+        Console.WriteLine(Abs(a));
+
+    }
+
+    static int Abs(int a)
+    {
+        return a*((2*a+1)%2);
+    }
+}
